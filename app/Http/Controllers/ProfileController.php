354bbab6838a5f430ac684dec468a4436733e72c
David García <ccristhiangarcia@gmail.com>
2024-05-05 12:22:32 -0500

    Complete all tasks of the test

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Validation\Rules;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
@@ -15,6 +18,22 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'], 
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['sometimes', 'confirmed', Rules\Password::defaults()]
+        ]);
+
+        $user = Auth::user();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        if ($request->filled('password')) {
+            $user->password = bcrypt($request->password);
+        }
+
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
